877a06988579305868ee355009510f6018deb7b6
nimmjnim <52564524+nimmjnim@users.noreply.github.com>
2019-07-21 20:28:49 -0700

    add all

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <pcap.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <linux/if_ether.h>
+
+void usage() {
+  printf("syntax: pcap_test <interface>\n");
+  printf("sample: pcap_test wlan0\n");
+}
+
+void print_MAC(const u_char* MAC){
+    printf("%02X:%02X:%02X:%02X:%02X:%02X\n", MAC[0], MAC[1], MAC[2], MAC[3], MAC[4], MAC[5]);
+}
+
+void print_ip(ulong IP){
+    uint32_t Ip= ntohl((uint32_t)IP);
+    printf("%u.%u.%u.%u\n", (Ip & 0xff000000) >> 24, (Ip & 0x00ff0000) >> 16, (Ip & 0x0000ff00) >> 8, (Ip & 0x000000ff));
+}
+
+void print_TCP(u_short TCP){
+    printf("%d\n", ntohs(TCP));
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    usage();
+   return -1;
+  }
+
+  char* dev = argv[1];
+  char errbuf[PCAP_ERRBUF_SIZE];
+  pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+  if (handle == NULL) {
+    fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
+    return -1;
+  }
+
+  while (true) {
+    struct pcap_pkthdr* header;
+    const u_char* packet;
+    int res = pcap_next_ex(handle, &header, &packet);
+    struct ethhdr *Ether;
+    struct ip *IP;
+    struct tcphdr *TCP;
+
+    if (res == 0) continue;
+    if (res == -1 || res == -2) break;
+
+    Ether = (struct ethhdr *)packet;
+    packet += 14;
+    IP = (struct ip *)packet;
+    int hl = IP->ip_hl*4;
+    packet += hl;
+    TCP = (struct tcphdr *)packet;
+
+    printf("--------------------------------------------------\n");
+    printf("DMAC: ");
+    print_MAC(Ether->h_dest);
+    printf("SMAC: ");
+    print_MAC(Ether->h_source);
+    if(Ether->h_proto == 0x0008){
+        printf("Dip: ");
+        print_ip(IP->ip_dst.s_addr);
+        printf("Sip: ");
+        print_ip(IP->ip_src.s_addr);
+        if(IP->ip_p == 0x6){
+            printf("DPort: ");
+            print_TCP(TCP->dest);
+            printf("SPort: ");
+            print_TCP(TCP->source);
+            if(ntohs(IP->ip_len)-hl-TCP->th_off*4 != 0){
+                int len = ntohs(IP->ip_len)-hl-TCP->th_off*4;
+                packet += TCP->th_off*4;
+                printf("TCP data: ");
+                for(int i = 0; i < 10 && i < len; i++) printf("%02x ", packet[i]);
+                printf("\n");
+            }
+        }
+    }
+    printf("--------------------------------------------------\n\n\n");
+  }
+
+  pcap_close(handle);
+  return 0;
+}
